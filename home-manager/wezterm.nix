524a8fafdf78aeca8cda3af95cfb8a9c16b108ca
L-PDufour <leonpierre.dufour@gmail.com>
2024-05-02 07:34:47 -0400

    wezterm/mux

--- home-manager/wezterm.nix
+++ home-manager/wezterm.nix
@@ -5,55 +5,85 @@
 
   programs.wezterm.enableZshIntegration = true;
   xdg.configFile."wezterm/wezterm.lua".text = ''
-    local wezterm = require 'wezterm'
+        local wezterm = require 'wezterm'
+        local mux = wezterm.mux
+        function is_default_startup(cmd)
+        if not cmd then
+        -- we were started with `wezterm` or `wezterm start` with
+        -- no other arguments
+        return true
+        end
+        if cmd.domain == "DefaultDomain" and not cmd.args then
+        -- Launched via `wezterm start --cwd something`
+        return true
+        end
+        -- we were launched some other way
+        return false
+        end
 
-    local xcursor_size = nil
-    local xcursor_theme = nil
+        wezterm.on('gui-startup', function(cmd)
+        if is_default_startup(cmd) then
+        -- for the default startup case, we want to switch to the unix domain instead
+        local unix = mux.get_domain("unix")
+        mux.set_default_domain(unix)
+        -- ensure that it is attached
+        unix:attach()
+        end
+        end)
 
-    local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"})
-    if success then
-      xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
-    end
+        local xcursor_size = nil
+        local xcursor_theme = nil
 
-    local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-size"})
-    if success then
-      xcursor_size = tonumber(stdout)
-    end
+        local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"})
+        if success then
+        xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
+        end
 
-    -- Define your WezTerm configuration
-    local config = {}
+        local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-size"})
+        if success then
+        xcursor_size = tonumber(stdout)
+        end
 
-    -- In newer versions of wezterm, use the config_builder which will
-    -- help provide clearer error messages
-    if wezterm.config_builder then
-        config = wezterm.config_builder()
-    end
+        -- Define your WezTerm configuration
+        -- In newer versions of wezterm, use the config_builder which will
+        -- help provide clearer error messages
+        if wezterm.config_builder then
+          config = wezterm.config_builder()
+        end
 
-    -- This is where you actually apply your config choices
-    config.default_prog = { '/run/current-system/sw/bin/zsh' }
-    config.colors = {}
-    config.colors.background = "#111111"
-    config.font = wezterm.font 'Fira Code'
-    config.font_size = 16.0
-    config.color_scheme = "Tokyo Night"
-    config.window_background_opacity = 0.95
-    config.enable_tab_bar = false
-    config.enable_wayland = true
-    config.use_fancy_tab_bar = false
-    config.enable_scroll_bar = true
-    config.term = "wezterm"
-    config.window_padding = {
-        left = 0,
-        right = 0,
-        top = 0,
-        bottom = 0,
+        -- This is where you actually apply your config choices
+        config.default_prog = { '/run/current-system/sw/bin/zsh' }
+        config.colors = {}
+        config.colors.background = "#111111"
+        config.font = wezterm.font 'Fira Code'
+        config.font_size = 16.0
+        config.color_scheme = "Tokyo Night"
+        config.window_background_opacity = 0.95
+        config.enable_tab_bar = false
+        config.enable_wayland = true
+        config.use_fancy_tab_bar = false
+        config.enable_scroll_bar = true
+        config.term = "wezterm"
+        config.window_padding = {
+          left = 0,
+          right = 0,
+          top = 0,
+          bottom = 0,
+        }
+        config.ssh_domains = {
+      {
+        name = 'homeserver',
+        remote_address = '192.168.50.173',
+        username = 'server',
+      },
     }
 
-    -- Merge the cursor configuration into the main configuration
-    config.xcursor_theme = xcursor_theme
-    config.xcursor_size = xcursor_size
+        -- Merge the cursor configuration into the main configuration
+        config.xcursor_theme = xcursor_theme
+        config.xcursor_size = xcursor_size
 
-    -- Return the merged configuration
-    return config
+
+        -- Return the merged configuration
+        return config
   '';
 }
