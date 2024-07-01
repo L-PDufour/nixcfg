acacf51a82d6a982a50525b621ecf4f969772f42
L-PDufour <leonpierre.dufour@gmail.com>
2024-07-01 14:48:05 -0400

    sway

--- home-manager/xorg.nix
+++ home-manager/xorg.nix
@@ -3,43 +3,61 @@
   lib,
   config,
   ...
-}: {
-  xsession.windowManager.i3.enable = true;
-  programs.i3status-rust.enable = true;
-  programs.i3status-rust.bars = {
-    default = {
-      blocks = [
+}: let
+  intToFloat = int: builtins.add (int * 1.0) 0.0; # Simple conversion to floating-point
+in {
+  xsession.windowManager.i3 = {
+    enable = true;
+    config = {
+      bars = [
         {
-          alert = 10.0;
-          block = "disk_space";
-          info_type = "available";
-          interval = 60;
-          path = "/";
-          warning = 20.0;
-        }
-        {
-          block = "memory";
-          format = " $icon mem_used_percents ";
-          format_alt = " $icon $swap_used_percents ";
-        }
-        {
-          block = "cpu";
-          interval = 1;
-        }
-        {
-          block = "load";
-          format = " $icon $1m ";
-          interval = 1;
-        }
-        {
-          block = "sound";
-        }
-        {
-          block = "time";
-          format = " $timestamp.datetime(f:'%a %d/%m %R') ";
-          interval = 60;
+          fonts = {
+            size = lib.mkForce (intToFloat config.stylix.fonts.sizes.terminal);
+          };
+          position = "top";
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-top.toml";
         }
       ];
+    };
+  };
+
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      top = {
+        blocks = [
+          {
+            alert = 10.0;
+            block = "disk_space";
+            info_type = "available";
+            interval = 60;
+            path = "/";
+            warning = 20.0;
+          }
+          {
+            block = "memory";
+            format = " $icon mem_used_percents ";
+            format_alt = " $icon $swap_used_percents ";
+          }
+          {
+            block = "cpu";
+            interval = 1;
+          }
+          {
+            block = "load";
+            format = " $icon $1m ";
+            interval = 1;
+          }
+          {
+            block = "sound";
+          }
+          {
+            block = "time";
+            format = " $timestamp.datetime(f:'%a %d/%m %R') ";
+            interval = 60;
+          }
+        ];
+      };
     };
   };
 }
